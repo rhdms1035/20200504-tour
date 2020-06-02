e62356c3edc80c7a81260b7a705a5c3301da7b0f
rhdms1035 <kkokkoning@naver.com>
2020-06-02 16:31:17 +0900

    6월2일슬라이드효과추가

--- js/myscript.js
+++ js/myscript.js
@@ -19,6 +19,7 @@ $(".to_top").on("click", function(){
 })
 
 // 화면사이즈가799커지면 클래스가 민티드
+var winWidth;
 function init() {
     var winWidth = $(window).width()
     if ( winWidth > 799 ) {
@@ -87,14 +88,45 @@ $(".place_list > li > a").on("click", function(e){
     var text = $(this).find("h3").text()
     var info = $(this).find("p").text()
     var alt = $(this).find("img").attr("alt")
+
+    if (winWidth <= 799 ) {
+        var litop = $(this).parent().offset().top;
+        $(".popupBox  .inner").css({
+                top:litop,
+                width:"80%",
+                left:"0%",
+                transform:"translate(0%)",
+                margin:"0  10%"
+        })
+}
     $(".popupBox").addClass("on")
     $(".popupBox .inner h3").text(text)
     $(".popupBox .inner p").text(info)
     $(".popupBox .inner div a").attr("href", href)
-    $(".popupBox .inner div img").attr("src", src).attr("alt", alt)
+    $(".popupBox .inner div img").attr("src", src).attr("alt", alt).attr("width", "100%")
 })
 
-// 엑스버튼을 누르면 창이 닫아짐
+// 팝업창 엑스버튼을 누르면 창이 닫아짐
 $(".popupBox button").on("click", function(){
     $(this).parents(".popupBox").removeClass("on")
+})
+
+// 메인슬라이드3개를 돌아가게 만듬.
+// 돌아가게 하려면 링크.slick.css와 스크립트slick.min.js을 꽂아놔야함
+$(".visualRoll").slick({
+    autoplay:true,     //기본값은 false, 자동재생
+    autoplaySpeed:3000,      //슬라이드 지연시간 3000->3초
+    dots:true,     //기본값은 false, 슬라이드번호버튼
+    speed:600,        //슬라이드 동작하는데 걸리는 시간
+    slidesToShow:1,        //보여지는 슬라이드의 수
+    slidesToScroll:1,       //넘어가는 슬라이드의 수
+    pauseOnHover:false,       //마우스오버시 슬라이드 멈춤 여부
+    pauseOnDotsHover:true,         //슬라이드번호버튼 위에 마우스오버시멈춤여부
+    pauseOnFocus:false,      //동그라미버튼 클릭시 자동실행 멈춤여부
+    cssEase:'linear',          //슬라이드 움직임 속도함수 (생략가능)
+    draggable:true,          //마우스드래그했을때 움직임 여부
+    fade:false,          //true값이면 페이드인페이드아웃 효과가 적용됨
+    arrows:true,      //기본값은 true이며 슬라이드 좌,우측 방향버튼
+    prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
 })
